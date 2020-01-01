23b683c7f713d0f76255cf8d7afc38c7708e1000
yanagitani <takanori.yanagitani@gmail.com>
2020-01-01 16:16:14 +0900

    ansible for local test

--- parse_environ_iovec.test.c
+++ parse_environ_iovec.test.c
@@ -4,6 +4,8 @@
 
 #include <stdio.h>
 
+#include <string.h>
+
 int environ2json(void* parameter, const struct iovec key, const struct iovec val){
   return 0;
 }
@@ -24,6 +26,7 @@ int main(int argc, char** argv){
     envbuf,
     1048576,
   };
+  strncpy(envbuf, "LANG=en_US.UTF-8\0USER=ubuntu\0HOME=/home/ubuntu\0TERM=screen\0SHELL=/bin/bash\0", 1048576);
   parse_environ_iovec(environ, NULL, environ2json, key, val);
   return 0;
 }
