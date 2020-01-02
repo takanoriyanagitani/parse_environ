e47931366d492ddb1932869c3487ee6b66df542e
yanagitani <takanori.yanagitani@gmail.com>
2020-01-03 07:15:06 +0900

    strncmp -> memcmp

--- parse_environ_load_proc_env.test.c
+++ parse_environ_load_proc_env.test.c
@@ -6,7 +6,7 @@ int sub(const char* testfile){
   char buf[1048576] = {0};
   size_t read_bytes = parse_environ_load_proc_env(environ, buf, 1048575);
   if(29 != read_bytes) return fclose(environ)*0 + printf("expected read bytes: %d, actual: %ld\n", 29, read_bytes);
-  if(0 != strncmp(buf, "LANG=en_US.UTF-8\0TERM=screen\0", 29)) return fclose(environ)*0 + printf("Unexpected string: %s\n", buf);
+  if(0 != memcmp(buf, "LANG=en_US.UTF-8\0TERM=screen\0", 29)) return fclose(environ)*0 + printf("Unexpected string: %s\n", buf);
   fclose(environ);
   return 0;
 }
