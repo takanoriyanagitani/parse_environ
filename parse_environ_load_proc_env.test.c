5d3497f29cd55547c8e61384f21b2a9e1544bd18
yanagitani <takanori.yanagitani@gmail.com>
2020-01-02 19:48:55 +0900

    switch -> if

--- parse_environ_load_proc_env.test.c
+++ parse_environ_load_proc_env.test.c
@@ -5,20 +5,8 @@ int sub(const char* testfile){
   if(NULL == environ) return printf("Unable to open environ file: %s\n", testfile);
   char buf[1048576] = {0};
   size_t read_bytes = parse_environ_load_proc_env(environ, buf, 1048575);
-  switch(read_bytes){
-    case 29: break;
-    default:
-      printf("expected read bytes: %ld, actual: %ld\n", read_bytes, read_bytes);
-      fclose(environ);
-      return -1;
-  }
-  switch(strncmp(buf, "LANG=en_US.UTF-8\0TERM=screen\0", 29)){
-    case 0: break;
-    default:
-      printf("expected buf: %s, actual: %s\n", "LANG=en_US.UTF-8\0TERM=screen", buf);
-      fclose(environ);
-      return -1;
-  }
+  if(29 != read_bytes) return fclose(environ)*0 + printf("expected read bytes: %d, actual: %ld\n", 29, read_bytes);
+  if(0 != strncmp(buf, "LANG=en_US.UTF-8\0TERM=screen\0", 29)) return fclose(environ)*0 + printf("Unexpected string: %s\n", buf);
   fclose(environ);
   return 0;
 }
